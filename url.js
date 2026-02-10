27be242f773953a4aeb286813345bd6a9268562f
losusanhr <losusanhr@gmail.com>
2026-02-10 21:46:24 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/AdBOR',
+	'https://www.surveycake.com/s/QOONA',
 ];
