c6745dca0023fc2b0bf4e2a1d9cad235487c4f50
losusanhr <losusanhr@gmail.com>
2026-02-21 18:24:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,13 +1,6 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
- * 2. 包起來的網址之間用逗號分隔
- */
-
-
+// url.js
+// 把要隨機分派的問卷連結放這裡（不需排序）
 window.urls = [
-	'https://www.surveycake.com/s/AdBOR',
-	'https://www.surveycake.com/s/QOONA',
+  'https://www.surveycake.com/s/AdBOR',
+  'https://www.surveycake.com/s/QOONA',
 ];
