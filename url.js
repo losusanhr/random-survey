ebb03efe5bba68565dca52bde63f1c29f79f4571
losusanhr <losusanhr@gmail.com>
2026-02-21 18:36:45 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,7 @@
 // url.js
+// 把問卷放這裡
+
 window.SURVEY_URLS = [
-  'https://www.surveycake.com/s/AdBOR',
-  'https://www.surveycake.com/s/QOONA',
+  "https://www.surveycake.com/s/AdBOR",
+  "https://www.surveycake.com/s/QOONA"
 ];
