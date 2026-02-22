965eae58ce8140a874415df34f30479bdb2eae7f
losusanhr <losusanhr@gmail.com>
2026-02-22 15:49:50 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,5 @@
 // url.js
-// 把問卷放這裡：問卷1的兩個版本
-window.SURVEY_URLS = [
-  { ver: "A", url: "https://www.surveycake.com/s/AdBOR" }, // 沒有不信任版本
-  { ver: "B", url: "https://www.surveycake.com/s/QOONA" }  // 不信任版本
-];;
+window.SURVEY1_URLS = [
+  "https://www.surveycake.com/s/AdBOR", // 版本A
+  "https://www.surveycake.com/s/QOONA"  // 版本B
+];
